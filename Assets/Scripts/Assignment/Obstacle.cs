469d38f0f84481c70603f5feb0f28235b69852c4
Ignacio Mosconi <nacho7577@hotmail.com>
2019-05-15 21:29:42 -0300

    Added Trees That Make the Illusion of Movement

--- Assets/Scripts/Assignment/Obstacle.cs
+++ Assets/Scripts/Assignment/Obstacle.cs
@@ -3,12 +3,12 @@ using PhysicsUtilities;
 
 public abstract class Obstacle : MonoBehaviour
 {
-    [SerializeField] float speed;
+    protected float speed;
 
     public abstract void Respawn();
 
     public void Move()
     {
-        PhysicalMotions.Linear(transform, transform.forward, speed);
+        PhysicalMotions.Linear(transform, -transform.up, speed);
     }
 }
---
+++ Assets/Scripts/Assignment/ObstacleManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Boundary
+{
+    Top, Bottom, Left, Right
+}
+
+public class ObstacleManager : MonoBehaviour
+{
+    static ObstacleManager instance;
+
+    [SerializeField] SpriteRenderer road;
+    [SerializeField] float treesSpeed;
+    [SerializeField] float minCarsSpeed;
+    [SerializeField] float maxCarsSpeed;
+
+    Dictionary<Boundary, float> roadBoundaries = new Dictionary<Boundary, float>();
+    Dictionary<Boundary, float> viewBoundaries = new Dictionary<Boundary, float>();
+    Obstacle[] obstacles;
+
+    void Awake()
+    {
+        if (Instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        Camera cam = Camera.main;
+
+        Vector3 leftLowerCorner = new Vector3(0f, 0f, 0f);
+        Vector3 rightUpperCorner = new Vector3(1f, 1f, 0f);
+        float leftRoadBound = -road.bounds.size.x * 0.5f;
+        float rightRoadBound = road.bounds.size.x * 0.5f;
+
+        roadBoundaries.Add(Boundary.Left, leftRoadBound);
+        roadBoundaries.Add(Boundary.Right, rightRoadBound);
+        roadBoundaries.Add(Boundary.Top, cam.ViewportToWorldPoint(rightUpperCorner).y);
+        roadBoundaries.Add(Boundary.Bottom, cam.ViewportToWorldPoint(leftLowerCorner).y);
+        
+        viewBoundaries.Add(Boundary.Left, cam.ViewportToWorldPoint(leftLowerCorner).x);
+        viewBoundaries.Add(Boundary.Right, cam.ViewportToWorldPoint(rightUpperCorner).x);
+        viewBoundaries.Add(Boundary.Top, cam.ViewportToWorldPoint(rightUpperCorner).y);
+        viewBoundaries.Add(Boundary.Bottom, cam.ViewportToWorldPoint(leftLowerCorner).y);
+
+        obstacles = GetComponentsInChildren<Obstacle>();
+    }
+
+    void Update()
+    {
+        foreach (Obstacle obstacle in obstacles)
+        {
+            obstacle.Move();
+            if (obstacle.transform.position.y < viewBoundaries[Boundary.Bottom])
+                obstacle.Respawn();
+        }
+    }
+
+    public float GetViewBound(Boundary boundary)
+    {
+        return viewBoundaries[boundary];
+    }
+
+    public float GetRoadBound(Boundary boundary)
+    {
+        return roadBoundaries[boundary];
+    }
+
+    public float GetRandomCarSpeed()
+    {
+        return (UnityEngine.Random.Range(minCarsSpeed, maxCarsSpeed));
+    }
+
+    public float TreesSpeed
+    {
+        get { return treesSpeed; }
+    }
+
+    public static ObstacleManager Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType<ObstacleManager>();
+                if (!instance)
+                {
+                    GameObject gameObj = new GameObject("Obstacle Manager");
+                    instance = gameObj.AddComponent<ObstacleManager>();
+                }
+            }
+
+            return instance;   
+        }
+    }
+}
